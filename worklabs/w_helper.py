78e32c5231a1fed5f679c6b93cbc65a7314df9de
bigE4 <ianjellsworth@gmail.com>
2025-02-25 15:56:14 -0700

    powershell script for ssh login and w_helper.py

---
+++ worklabs/w_helper.py
@@ -0,0 +1,9 @@
+nums1 = [20, 400, 273, 750, 1200, 6, 7, 8]
+nums = [0,1,2,3,4,5,6,7,8,9]
+
+for num in nums:
+    print('-----')
+    print(num % 4)
+    print(-3 * num % 4)
+    print(-7 * num % 4)
+    print(5 * num % 4)
